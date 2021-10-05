b98f6d8672803e53168e28a005908b0139031eea
nukjunk13 <cck_61_38@windowslive.com>
2021-10-05 13:10:02 +0700

    Get Problem

--- pages/admin/_home/problem.vue
+++ pages/admin/_home/problem.vue
@@ -1,32 +1,35 @@
 <template>
-            <div><PostProblemList :usersContacts="loadData" /></div>
+  <div><PostProblemList :usersContacts="loadData" /></div>
 </template>
 
 <script>
-import PostProblemList from '@/components/post/PostProblemList';
+import PostProblemList from '@/components/post/PostProblemList'
 import axios from 'axios'
 export default {
-    layout: 'navbarAdmin',
-    components:{
-        PostProblemList
-    },
-    data(){
-      return{
-        loadData:[]
-      }
-    },
-    asyncData(context){
-      return axios.get("https://createder-test-data-default-rtdb.firebaseio.com/usersContacts.json")
-      .then(res=>{
+  layout: 'navbarAdmin',
+  components: {
+    PostProblemList
+  },
+  data() {
+    return {
+      loadData: []
+    }
+  },
+  asyncData(context) {
+    //const date = new Date().toISOString().split('T')
+    return axios
+      .get(`https://createder-test-data-default-rtdb.firebaseio.com/usersContacts/${date[0]}.json`)
+      .then((res) => {
         //console.log(res.data);
-        const data=[];
-        for(const key in res.data){
-          data.push({...res.data[key],id:key})
+        const data = []
+        for (const key in res.data) {
+          data.push({ ...res.data[key], id: key })
+          console.log('Data Problem: ',data)
         }
-        return{
-          loadData:data
+        return {
+          loadData: data
         }
       })
-    }
+  }
 }
 </script>
