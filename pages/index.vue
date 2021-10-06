6c37a9f1263c517d9719e4c72f6ec246a0e35fb3
nukjunk13 <cck_61_38@windowslive.com>
2021-10-06 11:58:49 +0700

    Add clock digital in Home

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,44 @@
 <template>
   <div class="bg-cover bg-no-repeat min-h-screen flex items-center" style="background-image: url('/dFQROr7oWzulq5FZUEj5WDLeElRCjUaKapSNlWTetE95tnKuxiZH3tkcpzdGj4P2zNb.jpg');">
   <div class="max-w-md w-full space-y-8">
+    <div class="text-4xl font-black text-right text-black bg-white">{{ dateTime.hours }}:{{ dateTime.minutes }}:{{ dateTime.seconds }}</div>
     <p class="text-6xl font-black text-right text-white">Welcome</p>
   </div>
   </div>
 </template>
 
 <script>
+const date = new Date();
 export default {
   layout: 'navbarUser',
+  data() {
+    return {
+      dateTime: {
+        hours: date.getHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds(),
+      },
+      timer: undefined,
+    };
+  },
+  methods: {
+    setDateTime() {
+      const date = new Date();
+      this.dateTime = {
+        hours: date.getHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds(),
+      };
+    },
+  },
+  beforeMount() {
+    this.timer = setInterval(this.setDateTime, 1000);
+  },
+  beforeUnmount() {
+    clearInterval(this.timer);
+  },
+};
+</script>
 }
 </script>
 <style>
